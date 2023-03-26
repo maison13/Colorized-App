5e5a3de3d0404984296d0c936d486e4cd891a252
Vladimir <smile_friend@mail.ru>
2023-03-26 18:09:07 +0300

    setup View Controller

--- Colorized App/ViewController.swift
+++ Colorized App/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+        
+ 
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
+    
 }
 
