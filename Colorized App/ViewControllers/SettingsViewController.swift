86e33793ba2ca7545fe74fbfcdb17c153e095bc5
Vladimir <smile_friend@mail.ru>
2023-04-13 04:30:39 +0300

    setup length of textfield

--- Colorized App/ViewControllers/SettingsViewController.swift
+++ Colorized App/ViewControllers/SettingsViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 final class SettingsViewController: UIViewController {
-
+   
+    // MARK: - IBOutlet
     @IBOutlet weak var colorMixtureView: UIView!
     
     @IBOutlet weak var redLabel: UILabel!
@@ -19,30 +20,34 @@ final class SettingsViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    @IBOutlet var textFieldButtons: [UITextField]!
     
     @IBOutlet weak var redTextField: UITextField!
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
-    
-    
+   
+    // MARK: - public properties
     var backgroundColor: UIColor!
     unowned var delegate: SettingsViewControllerDelegate!
-        
+    
+  
+    // MARK: - initialization
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAllValueOfColors()
         setupResultOfColorView()
         
+        for button in textFieldButtons {
+            addDoneButton(textField: button)
+        }
+        
         redTextField.delegate = self
         greenTextField.delegate = self
         blueTextField.delegate = self
         
-        
-        
-        
-     }
-        
+    }
     
+    // MARK: - IBAction
     @IBAction func doneButtonAction(_ sender: UIButton) {
         delegate.setBackgroundColor(for: colorMixtureView.backgroundColor ?? backgroundColor)
         dismiss(animated: true)
@@ -63,22 +68,30 @@ final class SettingsViewController: UIViewController {
         setupResultOfColorView()
     }
     
+    // MARK: - Override methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+
+    }
     
+   // MARK: - Private methods
     private func setupAllValueOfColors() {
+        redSlider.value = Float(backgroundColor.rgba.red)
+        greenSlider.value = Float(backgroundColor.rgba.green)
+        blueSlider.value = Float(backgroundColor.rgba.blue)
+        
         redLabel.text = string(from: redSlider)
         greenLabel.text = string(from: greenSlider)
         blueLabel.text = string(from: blueSlider)
         
-        redTextField.text = String(format: "%.2f", backgroundColor.rgba.red)
-        greenTextField.text = String(format: "%.2f", backgroundColor.rgba.green)
-        blueTextField.text = String(format: "%.2f", backgroundColor.rgba.blue)
+        redTextField.text = string(from: redSlider)
+        greenTextField.text = string(from: greenSlider)
+        blueTextField.text = string(from: blueSlider)
         
-        redSlider.value = Float(backgroundColor.rgba.red)
-        greenSlider.value = Float(backgroundColor.rgba.green)
-        blueSlider.value = Float(backgroundColor.rgba.blue)
-
+        
     }
-        
+    
     private func setupResultOfColorView() {
         colorMixtureView.layer.cornerRadius = 25
         colorMixtureView.backgroundColor = UIColor(
@@ -87,14 +100,44 @@ final class SettingsViewController: UIViewController {
             blue: CGFloat(blueSlider.value),
             alpha: CGFloat(1.0)
         )
-       
     }
-    
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
     
-   
+    private func showAlert(withTitle title: String, andMessage message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAktion = UIAlertAction(title: "ok", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAktion)
+        present(alert, animated: true)
+        
+    }
+    private func addDoneButton(textField: UITextField) {
+        let keypadToolbar: UIToolbar = UIToolbar()
+        let done = UIBarButtonItem(
+            title: "Done",
+            style: UIBarButtonItem.Style.done,
+            target: textField,
+            action: #selector(UITextField.resignFirstResponder)
+        )
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        keypadToolbar.sizeToFit()
+        keypadToolbar.setItems([flexibleSpace, done], animated: false)
+        textField.inputAccessoryView = keypadToolbar
+    }
+        /*private func checkValue(_ sender: UITextField) {
+            let textLength = sender.text?.count ?? 0
+            if textLength > 4 {
+                showAlert(withTitle: "Ошибка ввода", andMessage: "Диапазон значений должен быть от 0 до 1")
+            }
+            
+        }*/
     
 }
 // MARK: - UITextFieldDelegate
@@ -105,6 +148,7 @@ extension SettingsViewController: UITextFieldDelegate {
         
         switch textField {
         case redTextField:
+            
             redLabel.text = String(numberValue)
             redSlider.value = numberValue
         case greenTextField:
@@ -114,8 +158,21 @@ extension SettingsViewController: UITextFieldDelegate {
             blueLabel.text = String(numberValue)
             blueSlider.value = numberValue
         }
+        setupResultOfColorView()
     }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if range.length + range.location > textField.text?.count ?? 0 {
+            return false
+        }
+        let newLimit = (textField.text?.count)! + string.count - range.length
+        return newLimit <= 4
+        
+    }
+
+    
+    
 }
+
 // MARK: - return RGBA from UIColor
 extension UIColor {
     var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
@@ -128,4 +185,6 @@ extension UIColor {
         return (red, green, blue, alpha)
     }
 }
+
+
 
